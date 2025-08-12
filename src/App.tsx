b9d7e8d877f195b587bbdfec2c83f45b207ccca9
David Gómez <david.gomez3@utp.ac.pa>
2025-08-11 22:11:39 -0500

    chore: initial commit with React, Tailwind, and shadcn setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import { useState } from 'react'
+
+function App() {
+  return (
+    <>
+      CoworkingReservation
+    </>
+  )
+}
+
+export default App

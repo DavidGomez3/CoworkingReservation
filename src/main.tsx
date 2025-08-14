b9e61591c0d6a4c1e33ff363c234851e2b741ca2
David Gómez <david.gomez3@utp.ac.pa>
2025-08-13 23:07:01 -0500

    chore(main): add TanStack Query client wrapper

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
+// src/main.ts
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import App from './App'
 import './index.css'
-import App from './App.tsx'
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      staleTime: 60_000,   // 1 min de datos frescos
+      refetchOnWindowFocus: false,
+    },
+    mutations: {
+      retry: 0,
+    },
+  },
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>,
 )
